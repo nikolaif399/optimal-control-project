Added custom standing PD controller to verify torque signs are correct

keras/LeggedRobot/TestDDPGPolicy.py
@@ -1,4 +1,4 @@
-0#from keras.models import load_model
+#from keras.models import load_model
 import numpy as np
 import pybullet as pb
 import pybullet_utils.bullet_client as bc
@@ -12,25 +12,49 @@ from envs.spirit_env import SpiritEnv
 
 # Setup PyBullet Simulation
 
-env = SpiritEnv(True,True)
+env = SpiritEnv(True,False)
 env.seed(123)
 env.reset()
+
+hip_kp = 0.5
+hip_kd = 0.05
+knee_kp = 0.5 # Action signal per error radian
+knee_kd = 0.05
 
 while(env.stepCount < 5000):
 	start = time.time()
 
-	hip_action = env.hip_nom_pos * np.ones((4,1))
-	knee_action = env.knee_nom_pos * np.ones((4,1))
-	
+	#hip_action = env.hip_nom_pos * np.ones((4,1))
+	#knee_action = env.knee_nom_pos * np.ones((4,1))
+
+	joint_pos = env.getJointPositions()
+	abd_pos = joint_pos[0:4,:]
+	hip_pos = joint_pos[4:8,:]
+	knee_pos = joint_pos[8:12,:]
+
+	knee_error = env.knee_nom_pos - knee_pos
+	hip_error = env.hip_nom_pos - hip_pos
+
+	joint_vel = env.getJointVelocities()
+	abd_vel = joint_vel[0:4,:]
+	hip_vel = joint_vel[4:8,:]
+	knee_vel = joint_vel[8:12,:]
+
+	hip_derror = -hip_vel
+	knee_derror = -knee_vel
+
+	hip_action = hip_kp * hip_error + hip_kd * hip_derror
+	knee_action = knee_kp * knee_error + knee_kd * knee_derror
+
 	action = np.vstack((hip_action, knee_action))
 	env.step(action)
 
 	appliedTorques = env.getJointTorques()
-	print("Hip Torques: ")
-	print(appliedTorques[4:8,:].T)
+	#print("Hip Torques: ")
+	#print(appliedTorques[4:8,:].T)
 
-	print("Knee Torques: ")
-	print(appliedTorques[8:12,:].T)
+	#print("Knee Torques: ")
+	#print(appliedTorques[8:12,:].T)
 
 	left = env.tstep - (time.time() - start)
 	if left > 0:

keras/LeggedRobot/envs/spirit_env.py
@@ -66,7 +66,7 @@ class SpiritEnv(gym.Env):
     self.stepCount = 0
 
 
-  def scaleAction(self, action):
+  def action2Torques(self, action):
     # Action scaled from ~ -1 to 1 (tanh output layer, random noise attached for exploration)
     action_column = action.reshape(self.num_acts,1)
     action_clipped = np.clip(action_column, -1, 1)
@@ -89,7 +89,7 @@ class SpiritEnv(gym.Env):
                                  targetPositions=action[4:8,:])
     
     else:
-      self.appliedTorques = self.scaleAction(action)
+      self.appliedTorques = self.action2Torques(action)
       hipTorques = self.appliedTorques[0:4,:]
       kneeTorques = self.appliedTorques[4:8,:]
       